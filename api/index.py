cdb97dd036b84b3337cfd6714251175a15d5ee1a
Andrei Li <andreiliphd@gmail.com>
2024-12-14 17:47:58 +0300

    envs

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
 import pprint
+import redis
 from flask import Flask, request, jsonify
+import os
+import time
+
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def home(): 
+    r = redis.Redis(
+    host=os.getenv("HOST"),
+    port=6379,
+    password=os.getenv("PASSWORD"),
+    ssl=True
+    )    
+    r.set(time.time(), str(request.json))
     data = request.json
     return jsonify(data)
 
