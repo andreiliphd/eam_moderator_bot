9c957fd30c6495d2425da566904bef10b9325e83
Andrei Li <andreiliphd@gmail.com>
2024-12-15 16:22:55 +0300

    UPDATE

--- api/index.py
+++ api/index.py
@@ -1,50 +1,156 @@
-import pprint
-import redis
-import json
-from flask import Flask, request, jsonify
+import asyncio
+import html
 import os
-import time
-import telebot
-from dotenv import load_dotenv
-from telebot.util import quick_markup
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import logging
+from dataclasses import dataclass
+from http import HTTPStatus
 
-load_dotenv()  # take environment variables from .env.
+import uvicorn
+from asgiref.wsgi import WsgiToAsgi
+from flask import Flask, Response, abort, make_response, request
 
-app = Flask(__name__)
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application,
+    CallbackContext,
+    CommandHandler,
+    ContextTypes,
+    ExtBot,
+    TypeHandler,
+)
 
-bot = telebot.TeleBot(os.getenv("API_TOKEN"))
-bot_2 = telebot.TeleBot(os.getenv("API_TOKEN_2"))
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-r = redis.Redis(
-host=os.getenv("HOST"),
-port=6379,
-password=os.getenv("PASSWORD"),
-ssl=True
-)    
+logger = logging.getLogger(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home(): 
-    data = request.json
-    chat = data['message']['chat']['id']
-    try:
-        r.set(data['update_id'], data['message']['from']['username'] + ' - ' + data['message']['text'])
-    except KeyError:
-        pass
-    bot.delete_message(chat, data['message']['message_id'])
-    return jsonify(data)
+# Define configuration constants
+# URL = "https://domain.tld"
+# ADMIN_CHAT_ID = 957931698
+# PORT = 8000
+# TOKEN = "123:ABC"  # nosec B105
 
-@app.route('/manage', methods=['GET', 'POST'])
-def do():
-    data = request.json
-    keys = r.keys('*')
-    values = r.mget(keys)
-    for i in range(len(keys)):
-        markup = telebot.types.InlineKeyboardMarkup()
-        btn_my_site= telebot.types.InlineKeyboardButton(text=values[i], callback_data=keys[i])
-        markup.add(btn_my_site)    
-        bot_2.send_message(chat_id = data['message']['chat']['id'], text = "Одобрите пост: ", reply_markup = markup)
-    return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+@dataclass
+class WebhookUpdate:
+    """Simple dataclass to wrap a custom update type"""
+
+    user_id: int
+    payload: str
+
+
+class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
+    """
+    Custom CallbackContext class that makes `user_data` available for updates of type
+    `WebhookUpdate`.
+    """
+
+    @classmethod
+    def from_update(
+        cls,
+        update: object,
+        application: "Application",
+    ) -> "CustomContext":
+        if isinstance(update, WebhookUpdate):
+            return cls(application=application, user_id=update.user_id)
+        return super().from_update(update, application)
+
+
+async def start(update: Update, context: CustomContext) -> None:
+    """Display a message with instructions on how to use this bot."""
+    payload_url = html.escape(f"{os.getenv("URL")}/submitpayload?user_id=<your user id>&payload=<payload>")
+    text = (
+        f"To check if the bot is still running, call <code>{os.getenv("URL")}/healthcheck</code>.\n\n"
+        f"To post a custom update, call <code>{payload_url}</code>."
+    )
+    await update.message.reply_html(text=text)
+
+
+async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
+    """Handle custom updates."""
+    chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
+    payloads = context.user_data.setdefault("payloads", [])
+    payloads.append(update.payload)
+    combined_payloads = "</code>\n• <code>".join(payloads)
+    text = (
+        f"The user {chat_member.user.mention_html()} has sent a new payload. "
+        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
+    )
+    await context.bot.send_message(chat_id=os.getenv("ADMIN_CHAT_ID"), text=text, parse_mode=ParseMode.HTML)
+
+
+async def main() -> None:
+    """Set up PTB application and a web application for handling the incoming requests."""
+    context_types = ContextTypes(context=CustomContext)
+    # Here we set updater to None because we want our custom webhook server to handle the updates
+    # and hence we don't need an Updater instance
+    application = (
+        Application.builder().token(os.getenv("TOKEN")).updater(None).context_types(context_types).build()
+    )
+
+    # register handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
+
+    # Pass webhook settings to telegram
+    await application.bot.set_webhook(url=f"{os.getenv("URL")}/telegram", allowed_updates=Update.ALL_TYPES)
+
+    # Set up webserver
+    flask_app = Flask(__name__)
+
+    @flask_app.post("/telegram")  # type: ignore[misc]
+    async def telegram() -> Response:
+        """Handle incoming Telegram updates by putting them into the `update_queue`"""
+        await application.update_queue.put(Update.de_json(data=request.json, bot=application.bot))
+        return Response(status=HTTPStatus.OK)
+
+    @flask_app.route("/submitpayload", methods=["GET", "POST"])  # type: ignore[misc]
+    async def custom_updates() -> Response:
+        """
+        Handle incoming webhook updates by also putting them into the `update_queue` if
+        the required parameters were passed correctly.
+        """
+        try:
+            user_id = int(request.args["user_id"])
+            payload = request.args["payload"]
+        except KeyError:
+            abort(
+                HTTPStatus.BAD_REQUEST,
+                "Please pass both `user_id` and `payload` as query parameters.",
+            )
+        except ValueError:
+            abort(HTTPStatus.BAD_REQUEST, "The `user_id` must be a string!")
+
+        await application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
+        return Response(status=HTTPStatus.OK)
+
+    @flask_app.get("/healthcheck")  # type: ignore[misc]
+    async def health() -> Response:
+        """For the health endpoint, reply with a simple plain text message."""
+        response = make_response("The bot is still running fine :)", HTTPStatus.OK)
+        response.mimetype = "text/plain"
+        return response
+
+    webserver = uvicorn.Server(
+        config=uvicorn.Config(
+            app=WsgiToAsgi(flask_app),
+            port=os.getenv("PORT"),
+            use_colors=False,
+            host="0.0.0.0",
+        )
+    )
+
+    # Run application and webserver together
+    async with application:
+        await application.start()
+        await webserver.serve()
+        await application.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
