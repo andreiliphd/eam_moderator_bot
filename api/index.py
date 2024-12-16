ace94aa0df7c99a15a6fe0847d19aefeacb72b2b
Andrei Li <andreiliphd@gmail.com>
2024-12-16 19:26:54 +0300

    DFOKE

--- api/index.py
+++ api/index.py
@@ -1,22 +1,20 @@
 import os
 import logging
-from sanic import Sanic
-from sanic.response import text
+from flask import Flask, request
 
-app = Sanic("eam_moderator")
-
+app = Flask(__name__)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-
-@app.get("/")
-async def hello_world(request):
-    logger.log(logging.WARNING, str(request))
-    return text("Hello, world.")
+@app.route("/", methods = ["GET", "POST"])
+def entry():
+    data = request.json
+    logger.log(logging.WARNING, str(data))
+    return str(request.json)
