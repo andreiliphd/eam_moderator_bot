002a966c27b9a9438ce002027f5b6e20bbdd63f6
Andrei Li <andreiliphd@gmail.com>
2024-12-16 19:59:58 +0300

    DFD

--- api/index.py
+++ api/index.py
@@ -13,6 +13,16 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+def telegram_url_builder(method, **kwargs)  
+    basic = "https://api.telegram.org/bot" + os.getenv("TOKEN") + "/" + str(method) + "?"
+    for k, val in kwargs.items():
+        print("%s == %s" % (k, val))
+        basic += k + "=" + val + "&"
+    basic = basic[-1]
+    logger.log(logging.WARNING, str(basic)) 
+    return basic
+
+
 @app.route("/", methods = ["GET", "POST"])
 def entry():
     data = request.json
