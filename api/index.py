35f42df8eb5e15cbc04ce3e550ef2b76eaa64d8d
Andrei Li <andreiliphd@gmail.com>
2024-12-16 19:15:18 +0300

    UOPO

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 import os
 import logging
-import uvicorn
 from telegram import Bot
-from fastapi import FastAPI, Request
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic("eam_moderator")
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -13,13 +16,9 @@ logging.getLogger("httpx").setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
 bot = Bot(token=os.getenv("TOKEN"))
 
 @app.get("/")
-async def root(request: Request):
-    logger.log(logging.error, str(request.json))
-    return {"message": "Hello from FastAPI!", "req": request.json}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+async def hello_world(request):
+    logger.log(logging.WARNING, str(request))
+    return text("Hello, world.")
