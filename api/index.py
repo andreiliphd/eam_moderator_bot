7c1e94131593087974a8b699fab52b6b9c754a1d
Andrei Li <andreiliphd@gmail.com>
2024-12-16 21:17:50 +0300

    NEBOT

--- api/index.py
+++ api/index.py
@@ -8,10 +8,8 @@ app = Flask(__name__)
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
 
 redis = Redis(url=os.getenv("HOST_REDIS"), token=os.getenv("PASSWORD_REDIS"))
